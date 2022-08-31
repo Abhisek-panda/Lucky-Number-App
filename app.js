cb715647dd614806f06d591147ec4572b980ed13
Abhisek-panda <abhisekpanda789@gmail.com>
2022-08-31 23:48:40 +0530

    added HTML, JS

---
+++ app.js
@@ -0,0 +1,32 @@
+const birthdayBox = document.querySelector("#birthday-box");
+const luckyBox = document.querySelector("#lucky-box");
+const button = document.querySelector("#btn")
+const luckyPara = document.querySelector("#lucky-para")
+
+
+function userData(sum, luckyBox) {
+    if (sum % luckyBox===0) {
+        luckyPara.innerText = "Your are going to a Panda";
+    }else {
+        luckyPara.innerText = "your are a gurshra";
+    }
+}
+
+function checkBirthdayLucky(){
+    const dob = birthdayBox.value;
+    const sum = calculateValue(dob);
+    userData(sum, luckyBox.value);
+}
+
+
+function calculateValue(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0 
+    for (let i=0; i<dob.length; i++) {
+        sum = sum + Number(dob.charAt(i))
+    }
+    return sum;
+}
+
+
+button.addEventListener("click", checkBirthdayLucky);
